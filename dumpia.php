434f08fc44b981fe94a3fa34d25f171576452e09
kenny <0@kenny.cat>
2018-10-18 16:56:18 +0200

    baby steps

--- dumpia.php
+++ dumpia.php
@@ -3,19 +3,64 @@ class Dumpia {
 	private $key;
 	private $fanclub;
 
+	const CURL_DEBUG = false;
+
 	const API_BASE = "https://fantia.jp/api/v1";
 	const API_POSTS = "/posts/";
 	const API_FANCLUBS = "/fanclubs/";
 
+	const STR_STARTUP = "dumpia - v0 - https://github.com/itskenny0/dumpia";
+
 	const ERR_USAGE = "Usage: php dumpia.php --fanclub 1880 --key AbCdEfGhI31Fjwed234";
+	const ERR_API_NO_JSON = "Invalid API response (JSON decode failed) - API said: ";
+
+	/* --------------------------------------------- */
+
+	private static function log($msg) {
+		$pid = getmypid();
+		$date = date("r");
+
+		$line = "[$date][$pid] $msg" . PHP_EOL;
+		echo $line;
+	}
+
+	/* --------------------------------------------- */
 
 	public function __construct($key, $fanclub) {
 		$this->key = $key;
 		$this->fanclub = $fanclub;
 	}
 
+	private function fetch($type, $id) {
+		self::log("cURL: " . self::API_BASE . $type . $id);
+		$c = curl_init(self::API_BASE . $type . $id);
+
+		curl_setopt($c, CURLOPT_COOKIE, '_session_id=' . $this->key . ';');
+		if(self::CURL_DEBUG) curl_setopt($c, CURLOPT_VERBOSE, 1);
+		curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
+
+		$out = curl_exec($c);
+		curl_close($c);
+
+		self::log("cURL: fetched - received " . strlen($out) . " bytes.");
+
+		$outJ = json_decode($out);
+		if(!is_object($outJ)) throw new Exception(self::ERR_API_NO_JSON . $out);
+		self::log("JSON decode OK");
+
+		return $outJ;
+	}
+
 	public function go() {
-		// stub
+		self::log(self::STR_STARTUP);
+
+		self::log("Fetching index ...");
+		$index = $this->fetch(self::API_FANCLUBS, $this->fanclub);
+
+		self::log("Downloaded index!");
+		self::log("-> Title: " . $index->fanclub->title);
+		self::log("-> " . count($index->fanclub->recent_posts) . " recent posts");
+		print_r($index);
 	}
 }
 
