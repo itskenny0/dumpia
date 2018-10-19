4257e5deacdb853139ba326045ebbaf3abe4a05a
kenny <0@kenny.cat>
2018-10-19 12:50:39 +0200

    post list downloading works now

--- dumpia.php
+++ dumpia.php
@@ -5,14 +5,21 @@ class Dumpia {
 
 	const CURL_DEBUG = false;
 
-	const API_BASE = "https://fantia.jp/api/v1";
-	const API_POSTS = "/posts/";
-	const API_FANCLUBS = "/fanclubs/";
+	const HTML_POST_URL_REGEX = "/\/posts\/[0-9]{1,8}/";
+
+	const API_POSTS = "https://fantia.jp/api/v1/posts/%s";
+	const API_FANCLUB = "https://fantia.jp/api/v1/fanclubs/%s";
+	const HTML_POSTLIST = "https://fantia.jp/fanclubs/%s/posts?page=%s";
+
+	const LOG_CURL_FETCHED = "cURL: Got HTTP/%s, received %s bytes.";
+	const LOG_POST_EXTRACT = "Extracted %s posts from page %s.";
 
 	const STR_STARTUP = "dumpia - v0 - https://github.com/itskenny0/dumpia";
 
 	const ERR_USAGE = "Usage: php dumpia.php --fanclub 1880 --key AbCdEfGhI31Fjwed234";
 	const ERR_API_NO_JSON = "Invalid API response (JSON decode failed) - API said: ";
+	const ERR_API_HTTP_NOK = "Got HTTP/%s - unable to fetch page.";
+	const ERR_API_NO_MATCHES = "Could not find any post URLs in the gallery. Possibly this fanclub has no posts or the format changed.";
 
 	/* --------------------------------------------- */
 
@@ -31,36 +38,75 @@ class Dumpia {
 		$this->fanclub = $fanclub;
 	}
 
-	private function fetch($type, $id) {
-		self::log("cURL: " . self::API_BASE . $type . $id);
-		$c = curl_init(self::API_BASE . $type . $id);
+	private function fetch($url) {
+		self::log("cURL: $url");
+		$c = curl_init($url);
 
 		curl_setopt($c, CURLOPT_COOKIE, '_session_id=' . $this->key . ';');
 		if(self::CURL_DEBUG) curl_setopt($c, CURLOPT_VERBOSE, 1);
 		curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
 
 		$out = curl_exec($c);
+		$httpcode = curl_getinfo($c, CURLINFO_HTTP_CODE);
 		curl_close($c);
 
-		self::log("cURL: fetched - received " . strlen($out) . " bytes.");
+		if($httpcode != 200) throw new Exception(sprintf(self::ERR_API_HTTP_NOK, $httpcode));
+
+		self::log(sprintf(self::LOG_CURL_FETCHED, $httpcode, strlen($out)));
+
+		return $out;
+	}
+
+	private function fetchJSON($type, $id) {
+		echo $url = sprintf($type, $id);
+		$out = $this->fetch($url);
 
 		$outJ = json_decode($out);
 		if(!is_object($outJ)) throw new Exception(self::ERR_API_NO_JSON . $out);
-		self::log("JSON decode OK");
+		self::log("JSON: Decode OK");
 
 		return $outJ;
+	}
+
+	private function fetchGalleryPage($page) {
+		$url = sprintf(self::HTML_POSTLIST, $this->fanclub, $page);
+		$html = $this->fetch($url);
+
+		$posts = $this->htmlExtractPosts($html);
+
+		self::log(sprintf(self::LOG_POST_EXTRACT, count($posts), $page));
+		return $posts;
+	}
+
+	private function htmlExtractPosts($html) {
+		preg_match_all(self::HTML_POST_URL_REGEX, $html, $out);
+
+		if(empty($out[0])) throw new Exception(self::ERR_API_NO_MATCHES);
+
+		return $out[0];
 	}
 
 	public function go() {
 		self::log(self::STR_STARTUP);
 
-		self::log("Fetching index ...");
-		$index = $this->fetch(self::API_FANCLUBS, $this->fanclub);
+		$page = 1; // start at pg1
+		$results = array();
 
-		self::log("Downloaded index!");
-		self::log("-> Title: " . $index->fanclub->title);
-		self::log("-> " . count($index->fanclub->recent_posts) . " recent posts");
-		print_r($index);
+		try {
+
+			while(true) {
+				self::log("Fetching page $page ...");
+				$out = $this->fetchGalleryPage($page);
+				$results = array_merge($results, $out);
+				$page++;
+			}
+
+		} catch (Exception $e) {
+
+			$ct = count($results);
+			self::log("No matches on page $page. Last fetchable page reached - downloading $ct posts.");
+
+		}
 	}
 }
 
@@ -68,7 +114,7 @@ $cliArgs = array("key:", "fanclub:");
 
 $options = getopt('', $cliArgs);
 if(!isset($options['key']) || !isset($options['fanclub'])) {
-	echo(Dumpia::ERR_USAGE);
+	echo(Dumpia::ERR_USAGE . PHP_EOL);
 	die();
 }
 
