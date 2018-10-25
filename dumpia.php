7d225a3cac151d818c3fccc98b9951768cfabdce
kenny <0@kenny.cat>
2018-10-25 13:24:43 +0200

    bugfixes, error handling

--- dumpia.php
+++ dumpia.php
@@ -15,17 +15,20 @@ class Dumpia {
 	/* Log format and strings */
 	const LOG_FORMAT = "[%s][%s] %s" . PHP_EOL; // $date, $pid, $msg
 	
-	const LOG_STARTUP = "dumpia - v0.3 - https://github.com/itskenny0/dumpia";
+	const LOG_STARTUP = "dumpia - v0.2 - https://github.com/itskenny0/dumpia";
 	const LOG_CURL_FETCHED = "cURL: Got HTTP/%s, received %s bytes.";
 	const LOG_POST_EXTRACT = "Extracted %s posts from page %s.";
 	const LOG_JSON_OK = "JSON: Decode OK";
 	const LOG_LAST_PAGE = "No matches on page %s. Last fetchable page reached.";
-	const LOG_DOWNLOAD_BEGIN = "Downloading %s posts ...";
+	const LOG_DOWNLOAD_BEGIN = "Trying %s posts ...";
 	const LOG_FETCH_PAGE = "Attempting to fetch page %s...";
 	const LOG_FETCH_METADATA = "Fetching metadata (JSON) for post %s ...";
 	const LOG_URL_LIST = "Downloading %s URLs for post %s...";
 	const LOG_NO_URL = "Unable to find URL for %s";
 	const LOG_CURL = "cURL: %s";
+	const LOG_NO_PHOTOS = "No photos found in post %s (post_content_photos is empty).";
+	const LOG_DOWNLOAD_FIN = "Download finished.";
+	const LOG_DOWNLOAD_SKIPPED = "The following posts had no downloadable photos and were skipped: %s";
 
 	const ERR_USAGE = "Usage: php dumpia.php --fanclub 1880 --key AbCdEfGhI31Fjwed234 --output /home/user/dumpia/ [--verbose]";
 	const ERR_API_NO_JSON = "Invalid API response (JSON decode failed) - API said: ";
@@ -34,7 +37,7 @@ class Dumpia {
 	
 	/* Extraction regexes */
 	const HTML_POST_URL_REGEX = "/\/posts\/(?<id>[0-9]{1,8})/";
-	const FILENAME_REGEX = "/\/(?<name>[^\/]+\.(jpg|jpeg|png|svg|bmp))/";
+	const FILENAME_REGEX = "/\/(?<name>[^\/]+\.(jpg|jpeg|png|svg|bmp))/i";
 
 	public function __construct($options) {
 		$this->key = $options['key'];
@@ -68,6 +71,7 @@ class Dumpia {
 		
 		self::log(sprintf(self::LOG_DOWNLOAD_BEGIN, $ct));
 
+		$emptyPosts = array(); // if posts are skipped because there is no content in it, the post ID will be added to this array
 		$urlsByPost = array();
 		$countTotal = 0;
 
@@ -75,6 +79,12 @@ class Dumpia {
 			if($this->verbose) self::log(sprintf(self::LOG_FETCH_METADATA, $id));
 
 			$out = $this->getPostPhotos($id); // get list of raw URLs from REST API
+			
+			if(empty($out)) {
+				if($this->verbose) self::log(sprintf(self::LOG_NO_PHOTOS, $id));
+				$emptyPosts[] = $id; // collect empty posts for output after completion
+				continue; // if no photos in post, skip to next one				
+			}
 
 			$count = count($out);
 			$countTotal += $count;
@@ -82,6 +92,9 @@ class Dumpia {
 
 			$this->download($this->output . '/' . $id, $out); // download images into a folder named after the post ID
 		}
+		
+		self::log(self::LOG_DOWNLOAD_FIN);
+		if(!empty($emptyPosts)) self::log(sprintf(self::LOG_DOWNLOAD_SKIPPED, implode(" ", $emptyPosts)));
 	}
 	
 	private static function log($msg) {
@@ -147,8 +160,11 @@ class Dumpia {
 		$results = array();
 		if(isset($out->post->thumb)) $results[] = $out->post->thumb->original ?: $out->post->thumb->main ?: $out->post->thumb->large ?: $out->post->thumb->medium; // cover image
 		
+		if(empty($out->post->post_contents)) return $results;
+		
 		foreach($out->post->post_contents as $c) {
-			if(empty($c->post_content_photos)) continue; // if no photos in post, skip to next one
+			if(empty($c->post_content_photos)) continue;
+			
 			foreach($c->post_content_photos as $i) {
 				$url = $i->url->original ?: $i->url->main ?: $i->url->large ?: $i->url->medium;
 				if(empty($url)) self::log(sprintf(self::LOG_NO_URL, $i->id));
@@ -168,7 +184,8 @@ class Dumpia {
 			preg_match(self::FILENAME_REGEX, $u, $out);
 
 			if(empty($out['name'])) {
-				echo "!";
+				echo $u;
+				echo "-";
 				continue;
 			}
 			
